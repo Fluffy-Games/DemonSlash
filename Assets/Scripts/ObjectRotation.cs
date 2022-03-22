89e002e09aed7fc11e30bc63d8b08db213221823
semiustunturk <semiustunturk@gmail.com>
2022-03-22 16:29:09 +0300

    Diamond

---
+++ Assets/Scripts/ObjectRotation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotation : MonoBehaviour
+{
+    public enum SpinType
+    {
+        xAxis,
+        yAxis,
+        zAxis
+    }
+    public SpinType spinType;
+
+    [SerializeField] float rotationSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(spinType == SpinType.yAxis)
+            transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+
+        if (spinType == SpinType.zAxis)
+            transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+    }
+}

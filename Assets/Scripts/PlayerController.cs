ca0840045d043c7ea006b8bff6a46418215675ae
Cagatay ONDER <cagatay200694@gmail.com>
2022-03-22 15:39:20 +0300

    push

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,24 +9,41 @@ public class PlayerController : MonoSingleton<PlayerController>
     private static readonly int Run = Animator.StringToHash("run");
     private static readonly int AttackIn = Animator.StringToHash("attackIn");
     private static readonly int AttackOut = Animator.StringToHash("attackOut");
+    private int swordSlashCount;
+    private bool oneSlash;
 
     public void StartLevel()
     {
+        swordSlashCount = 0;
         playerAnim.SetBool(Run, true);
     }
 
     public void StartAttack()
     {
-        StartCoroutine(Attack());
+        // StartCoroutine(Attack());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Obstacle obstacle = other.GetComponentInParent<Obstacle>();
+        Collectable collectable = other.GetComponentInParent<Collectable>();
+        
+        if (obstacle && !obstacle.oneSlash)
+        {
+            swordSlashCount++;
+            print(swordSlashCount.ToString());
+            playerAnim.SetTrigger(swordSlashCount %2 == 0 ? AttackIn: AttackOut);
+            obstacle.oneSlash = true;
+        }
     }
 
     // ReSharper disable once FunctionRecursiveOnAllPaths
-    private IEnumerator Attack()
-    {
-        yield return new WaitForSeconds(1.5f);
-        playerAnim.SetTrigger(AttackIn);
-        yield return new WaitForSeconds(1.5f);
-        playerAnim.SetTrigger(AttackOut);
-        StartCoroutine(Attack());
-    }
+    // private IEnumerator Attack()
+    // {
+    //     yield return new WaitForSeconds(1.5f);
+    //     playerAnim.SetTrigger(AttackIn);
+    //     yield return new WaitForSeconds(1.5f);
+    //     playerAnim.SetTrigger(AttackOut);
+    //     StartCoroutine(Attack());
+    // }
 }
